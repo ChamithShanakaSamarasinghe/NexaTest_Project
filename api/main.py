07dac1eedc6dda2c4af9cebb43c6f3bd1689266a
ChamithShanakaSamarasinghe <96052142+ChamithShanakaSamarasinghe@users.noreply.github.com>
2026-04-23 12:25:11 +0530

    fix fastapi runtime crash on vercel

--- api/main.py
+++ api/main.py
@@ -1,11 +1,7 @@
 from fastapi import FastAPI
-from mangum import Mangum
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "NexaTest is running"}
-
-# REQUIRED for Vercel
-handler = Mangum(app)
