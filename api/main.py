3251175d2c8bb98d5059cd4858eee4847b48e43c
ChamithShanakaSamarasinghe <96052142+ChamithShanakaSamarasinghe@users.noreply.github.com>
2026-04-23 12:14:59 +0530

    fix fastapi serverless handler for vercel

--- api/main.py
+++ api/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from mangum import Mangum
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "NexaTest API deployed successfully 🚀"}
+    return {"message": "NexaTest is running"}
+
+# THIS is required for Vercel
+handler = Mangum(app)
